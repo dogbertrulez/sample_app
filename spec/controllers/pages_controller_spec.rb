eeda3eb9c19cdfbbf3eeeb7b63a4ee8d35d155e8
Anirudh Shah <anirudh.shah@gmail.com>
2011-12-07 18:14:44 +0530

    Done with static content updates.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+      response.should have_selector("title", :content => "Sample App | Home")
     end
   end
 
@@ -13,6 +15,7 @@ describe PagesController do
     it "returns http success" do
       get 'contact'
       response.should be_success
+      response.should have_selector("title", :content => "Sample App | Contact")
     end
   end
 
@@ -20,7 +23,15 @@ describe PagesController do
     it "returns http success" do
       get 'about'
       response.should be_success
+      response.should have_selector("title", :content => "Sample App | About")
     end
   end
-
+  
+  describe "GET 'map'" do
+    it "returns http success" do
+      get 'map'
+      response.should be_success
+      response.should have_selector("title", :content => "Sample App | Map")
+    end
+  end
 end
